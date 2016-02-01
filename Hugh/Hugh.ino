0006a1e0a8a742e8cd8bd058b10db4369f1c5724
litlw <hasaunderritchie@gmail.com>
2016-02-01 14:24:36 -0500

    updated some junk

--- Hugh/Hugh.ino
+++ Hugh/Hugh.ino
@@ -1,11 +1,40 @@
 // Hugh's Arduino
 
+const int ps = 10; // this is the pressure sensor
+const int dcm = 9;  // this is the motor. 
+const int timer = 300; // this is how much time for delay. 
+int var = 0; // placeholder
+int go = 30;
+
+
 void setup() {
   // put your setup code here, to run once:
+serial.Begin(9600);
+  pinMode (ps, INPUT);
+  pinMode (dcm, OUTPUT);
 
 }
 
 void loop() {
+
+  var = analogRead(ps); //var is equal to whatever the sensor reads. 
+  serial.println(var);
+  vor = map(var, 0, 900, 0, 255);
+  float x = 0;
+  if (var > go){
+    x = 1;
+  } else {}
+
+  if (x == 1){
+    digitalWrite(dcm, HIGH);
+    delay(timer);
+    digitalWrite(dcm, LOW);
+    x = 0;
+  } else {}m
+
+  
   // put your main code here, to run repeatedly:
+
+  
 
 }
--- Melissa/Melissa.ino
+++ Melissa/Melissa.ino
@@ -1,11 +1,34 @@
 // Melissa's Arduino
+//this is the arduino with the servo arm swing thing. 
+#include <Servo.h>
+
+
+const int servo = 9;
+const int sensor = 10;
+int var = 0; // placeholder
+int go = 30;
+
+Servo myservo;
+
 
 void setup() {
   // put your setup code here, to run once:
+serial.begin(9600);
+myservo.attach(servo);
+pinMode(sensor, INPUT);
+
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  var = analogRead(sensor);
+  serial.println(var);
+  var = map(var, 0, 900, 0, 180);
+if (var > go){
+  myservo.write (180);
+}
+  
 
 }
